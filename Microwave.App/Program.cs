473a4c909ac035d57c3acc782344e4e1705845a2
camil <camil@DESKTOP-8M3PEC4>
2022-04-20 15:20:43 +0200

    unload integration test

--- Microwave.App/Program.cs
+++ Microwave.App/Program.cs
@@ -24,9 +24,11 @@ namespace Microwave.App
 
             Microwave.Classes.Boundary.Timer timer = new Timer();
 
+            Buzzer buzzer = new Buzzer(output);
+
             CookController cooker = new CookController(timer, display, powerTube);
 
-            UserInterface ui = new UserInterface(powerButton, timeButton, startCancelButton, door, display, light, cooker);
+            UserInterface ui = new UserInterface(powerButton, timeButton, startCancelButton, door, display, light, cooker,buzzer);
 
             // Finish the double association
             cooker.UI = ui;
