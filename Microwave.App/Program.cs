37d298273538cb53a7f4014e49e06a0f3b7026f8
CecilieNowak <78858829+CecilieNowak@users.noreply.github.com>
2022-04-21 15:46:52 +0200

    Konfigurering i program, powertube og userInterface med fields, property samt constructor injection

--- Microwave.App/Program.cs
+++ Microwave.App/Program.cs
@@ -6,6 +6,7 @@ namespace Microwave.App
 {
     class Program
     {
+       
         static void Main(string[] args)
         {
             Button startCancelButton = new Button();
@@ -18,7 +19,9 @@ namespace Microwave.App
 
             Display display = new Display(output);
 
-            PowerTube powerTube = new PowerTube(output);
+            int configMaxPower = 500;
+
+            PowerTube powerTube = new PowerTube(output, configMaxPower);
 
             Light light = new Light(output);
 
