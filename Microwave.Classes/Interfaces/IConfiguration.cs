ec7d0b97694200ed89ef01d2e52557b56e06de75
CecilieNowak <78858829+CecilieNowak@users.noreply.github.com>
2022-04-26 08:34:05 +0200

    interface til configuration, updated tests

---
+++ Microwave.Classes/Interfaces/IConfiguration.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microwave.Classes.Interfaces
+{
+    public interface IConfiguration
+    {
+        public int MaxPower { get; set; }
+    }
+}
