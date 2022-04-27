c12a3ef067ce2bb699549861898bffc3ae4de587
CecilieNowak <78858829+CecilieNowak@users.noreply.github.com>
2022-04-27 09:08:48 +0200

    configuration test

---
+++ Microwave.Test.Unit/ConfigurationTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microwave.Classes.Boundary;
+using NUnit.Framework;
+
+namespace Microwave.Test.Unit
+{
+    [TestFixture]
+    class ConfigurationTest
+    {
+
+
+        private Configuration uut;
+
+        [SetUp]
+        public void Setup()
+        {
+            uut = new Configuration(0); //default value
+        }
+
+        [Test]
+        public void SetMaxPower_ValueIsSetToExpectedValue()
+        {
+            uut.MaxPower = 100;
+            Assert.That(uut.MaxPower, Is.EqualTo(100));
+
+
+        }
+
+        [Test]
+        public void GetMaxPower_ReturnsExpectedValue()
+        {
+            uut.MaxPower = 100;
+            int value = uut.MaxPower;
+            Assert.That(uut.MaxPower, Is.EqualTo(value));
+
+        }
+
+
+    }
+}
